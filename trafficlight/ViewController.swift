7295e6d8554e12b5f96ed2a30cc7637fb79f8538
AlyJ <alina.jarmolica2@gmail.com>
2020-08-24 18:05:31 +0300

    Initial Commit

---
+++ trafficlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficlight
+//
+//  Created by alina.jarmolica on 24/08/2020.
+//  Copyright © 2020 alina.jarmolica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
